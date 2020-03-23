efa78b7faae7f501d0a131ed93ce773010281978
DinaMaskalik <62545926+DinaMaskalik@users.noreply.github.com>
2020-03-23 14:33:12 +0300

    Delete Main.java

--- homeWorkDZ1-master/src/by/academy/HomeWork/Task1/Main.java
+++
@@ -1,9 +0,0 @@
-package by.academy.HomeWork.Task1;
-
-public class Main {
-    public static void main(String[] args) {
-        CalculatorWithOperator calculator = new CalculatorWithOperator("4.1 + 15 * 7 + (28 / 5) ^ 2");
-        double  x = calculator.execute();
-
-    }
-}
